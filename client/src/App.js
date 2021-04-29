1dd9c85b4171d1bf73635dc04010e866f738c5c6
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-04-28 21:53:12 -0400

    create productScreen homeScreen routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import data from './data.js';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
+
 function App() {
   return (
+    <Router>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -13,49 +19,15 @@ function App() {
           <a href="/cart">Cart</a>
           <a href="/signin">Sign In</a>
         </div>
+        
       </header>
       <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
+    </Router>
   );
 }
 
