2500c09fe7e507de3fdd20b29d7b49c8dcf84542
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-05-01 16:12:54 -0400

    add items to cart and update the qty

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
-
+import {useSelector} from 'react-redux';
 
 function App() {
+  const cart = useSelector((state)=>state.cart);
+  const {cartItems} = cart;
   return (
     <Router>
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">
+          <Link className="brand" to="/">
             G-Tech Automation
-          </a>
+          </Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+          <Link to="/cart">Cart
+          {cartItems.length > 0 && (<span className="badge">{cartItems.length}</span>)}
+          </Link>
+          <Link to="/signin">Sign In</Link>
         </div>
         
       </header>
