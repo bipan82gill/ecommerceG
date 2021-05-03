8b3a62136774eb2d7d6084dc2a3384ea9cd54375
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-05-03 15:03:00 -0400

    build register screen

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -1,24 +1,30 @@
 import React,{ useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import { signin} from '../actions/userActions';
+import { register} from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen(props) {
+export default function RegisterScreen(props) {
+    const [name, setName]= useState('');
     const [email, setEmail]= useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const redirect = props.location.search
     ? props.location.search.split('=')[1]
     :'/';
-    const userSignin = useSelector((state)=> state.userSignin);
-    const {userInfo, loading, error} = userSignin;
+    const userRegister = useSelector((state)=> state.userRegister);
+    const {userInfo, loading, error} = userRegister;
 
     const dispatch = useDispatch();
 
     const submitHandler = (e) =>{
         e.preventDefault();
-        dispatch(signin(email,password));
+        if(password !== confirmPassword){
+            alert('Password and confirm-password are not match')
+        }else{
+        dispatch(register(name, email,password));
+        }
     }
     useEffect(()=>{
         if(userInfo){
@@ -29,10 +35,20 @@ export default function SigninScreen(props) {
         <div>
             <form className="form" onSubmit={submitHandler}>
                 <div>
-                    <h1>Sign In </h1>
+                    <h1>Create an Account</h1>
                     {loading && (<LoadingBox></LoadingBox>)}
                     {error && (<MessageBox className="variant">{error}</MessageBox>)
                 }
+                </div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                    type="text"
+                    id="name"
+                    placeholder="Enter name"
+                    required
+                    onChange={(e)=> setName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="email">Email address</label>
@@ -45,7 +61,7 @@ export default function SigninScreen(props) {
                     />
                 </div>
                 <div>
-                    <label htmlFor="password">Email password</label>
+                    <label htmlFor="password">Password</label>
                     <input 
                     type="password"
                     id="password"
@@ -55,14 +71,25 @@ export default function SigninScreen(props) {
                     />
                 </div>
                 <div>
+                    <label htmlFor="confirmPassword">Confirm password</label>
+                    <input 
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Enter confirm password"
+                    required
+                    onChange={(e)=> setConfirmPassword(e.target.value)}
+                    />
+                </div>
+                <div>
                     <label/>
-                    <button className="primary" type="submit">Sign in</button>
+                    <button className="primary" type="submit">Register</button>
                 </div>
                 <div>
                     <label/>
                     <div>
-                        New customer? {' '}
-                        <Link to ={`/register?redirect=${redirect}`}> create your account</Link>
+                        Already have an account ? {' '}
+                        <Link to={`/signin?redirect=${redirect}`}> Sign-In</Link>
+                        
                     </div>
                 </div>
 
