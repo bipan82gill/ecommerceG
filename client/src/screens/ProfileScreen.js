9c3060965b489cdc32c5335e615387e503f6a683
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-05-09 17:04:11 -0400

    update user profile information

--- client/src/screens/ProfileScreen.js
+++ client/src/screens/ProfileScreen.js
@@ -1,39 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { detailsUser } from '../actions/userActions';
+import { detailsUser, updateUserProfile } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
 
 
 export default function ProfileScreen() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+ 
     const userSignin = useSelector(state=> state.userSignin);
     const {userInfo} = userSignin;
     const userDetails = useSelector(state => state.userDetails);
     const {loading, error, user} = userDetails;
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile);
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate} = userUpdateProfile;
     const dispatch = useDispatch();
 useEffect(()=>{
-    dispatch(detailsUser(userInfo._id));
-
-}, [dispatch, userInfo._id]);
+    if(!user){
+        dispatch({type:USER_UPDATE_PROFILE_RESET});
+        dispatch(detailsUser(userInfo._id));
+    }else{
+        setName(user.name);
+        setEmail(user.email)
+    }
+  
+}, [dispatch, userInfo._id, user]);
 
 const submitHandler = (e)=>{
     e.preventDefault();
+    if(password !== confirmPassword){
+        alert('Password and Confirm Password are not match');
+    }else{
+        dispatch(updateUserProfile({userId: user._id, name, email,password}));
+    }
 }
     return (
         <div>
             <form className="form" onSubmit={submitHandler}>
                 <div><h1>User Profile</h1></div>
-                {loading? (<LoadingBox></LoadingBox>)
+                {loading ? (<LoadingBox></LoadingBox>)
                 : error ? (<MessageBox variant="danger">{error}</MessageBox>)
             : (
                 <>
+                {loadingUpdate && (<LoadingBox></LoadingBox>)}
+                {errorUpdate && (<MessageBox variant="danger">{error}</MessageBox>)}
+                {successUpdate && (<MessageBox variant ="success">Profile Updated Successfully</MessageBox>)}
                 <div>
                     <label htmlFor="name">Name</label>
                     <input
                     id="name"
                     type="text"
                     placeholder="Enter name"
-                    value={user.name}
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                     ></input>
                 </div>
                 <div>
@@ -42,7 +65,8 @@ const submitHandler = (e)=>{
                     id="email"
                     type="email"
                     placeholder="Enter email"
-                    value={user.email}
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                     ></input>
                 </div>
                 <div>
@@ -51,6 +75,7 @@ const submitHandler = (e)=>{
                     id="password"
                     type="passeord"
                     placeholder="Enter Password"
+                    onChange={(e)=>setPassword(e.target.value)}
                     ></input>
                 </div>
                 <div>
@@ -59,6 +84,7 @@ const submitHandler = (e)=>{
                     id="confirmPassword"
                     type="password"
                     placeholder="Enter confirm Password"
+                    onChange={(e)=>setConfirmPassword(e.target.value)}
                     ></input>
                 </div>
                 <div>
