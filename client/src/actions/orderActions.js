ee4d29096c564cf9a9ea6b6e39d433f0975f5cb7
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-05-12 21:18:04 -0400

    build order list screen

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -9,7 +9,10 @@ import {ORDER_CREATE_FAIL,
     ORDER_MINE_LIST_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_REQUEST,
-    ORDER_PAY_SUCCESS} from '../constants/orderConstants';
+    ORDER_PAY_SUCCESS,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+ORDER_LIST_FAIL} from '../constants/orderConstants';
 
 import {CART_EMPTY} from '../constants/cartConstants';
 import Axios from 'axios';
@@ -88,3 +91,20 @@ export const listOrdermine = () => async(dispatch, getState)=>{
         : error.message});
     }
 } 
+
+export const listOrders = () => async(dispatch, getState)=>{
+    dispatch({type:ORDER_LIST_REQUEST});
+    const {userSignin:{userInfo}} = getState();
+    try{
+        const {data} = await Axios.get(`/api/orders`,{
+            headers:{ Authorization: `Bearer ${userInfo.token}`}
+        });
+        dispatch({type:ORDER_LIST_SUCCESS, payload:data});
+    }catch(error){
+        dispatch({type:ORDER_LIST_FAIL, payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message});
+    }
+
+}
